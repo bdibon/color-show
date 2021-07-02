454ca9646a3885be84dccb57a626d88973b77a48
Boris DIBON <boris.dibon@gmail.com>
2021-07-02 11:09:57 +0200

    Add NavBar and Robot font

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
-import Slider from "rc-slider";
 import ColorBox from "./ColorBox";
-
-import "rc-slider/assets/index.css";
+import NavBar from "./NavBar";
 import "./Palette.css";
 
 export default class Palette extends Component {
@@ -23,16 +21,7 @@ export default class Palette extends Component {
     ));
     return (
       <div className="Palette">
-        {/* Navbar goes here */}
-        <div className="slider">
-          <Slider
-            defaultValue={level}
-            min={100}
-            max={900}
-            step={100}
-            onAfterChange={this.changeLevel}
-          />
-        </div>
+        <NavBar level={level} changeLevel={this.changeLevel} />
         <div className="Palette-colors">{colorBoxes}</div>
         {/* Footer */}
       </div>
