5955ba34b8613c558bdff183ff2bc35ffe8cd7f6
Boris DIBON <boris.dibon@gmail.com>
2021-07-06 19:27:39 +0200

    Add DragganleColorBox

---
+++ src/DraggableColorBox.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { withStyles } from "@material-ui/styles";
+
+const styles = {
+  root: {
+    cursor: "pointer",
+    display: "inline-block",
+    height: "25%",
+    margin: "0 auto",
+    marginBottom: "-3.9px",
+    position: "relative",
+    width: "20%",
+  },
+};
+
+function DraggableColorBox(props) {
+  const { color, classes } = props;
+  return (
+    <div className={classes.root} style={{ backgroundColor: color }}>
+      {color}
+    </div>
+  );
+}
+
+export default withStyles(styles)(DraggableColorBox);
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -12,6 +12,7 @@ import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import { Button } from "@material-ui/core";
 import { ChromePicker } from "react-color";
+import DraggableColorBox from "./DraggableColorBox";
 
 const drawerWidth = 400;
 
@@ -56,6 +57,7 @@ const useStyles = makeStyles((theme) => ({
   },
   content: {
     flexGrow: 1,
+    height: "calc(100vh - 64px)",
     padding: theme.spacing(3),
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
@@ -157,11 +159,9 @@ export default function NewPaletteForm() {
         })}
       >
         <div className={classes.drawerHeader} />
-        <ul>
-          {colors.map((color) => (
-            <li style={{ backgroundColor: color }}>{color}</li>
-          ))}
-        </ul>
+        {colors.map((color) => (
+          <DraggableColorBox key={color} color={color} />
+        ))}
       </main>
     </div>
   );
