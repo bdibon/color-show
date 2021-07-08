2ba04427491bc2ad14f1a7620d2e75281ac142ec
Boris DIBON <boris.dibon@gmail.com>
2021-07-08 15:39:27 +0200

    Allow palette peristance in localStorage

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import seedColors from "./seedColors";
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { palettes: seedColors };
+    const savedPalettes = JSON.parse(localStorage.getItem("palettes"));
+    this.state = { palettes: savedPalettes || seedColors };
     this.savePalette = this.savePalette.bind(this);
   }
 
@@ -20,7 +21,17 @@ export default class App extends Component {
   }
 
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
+  }
+
+  syncLocalStorage() {
+    window.localStorage.setItem(
+      "palettes",
+      JSON.stringify(this.state.palettes)
+    );
   }
 
   render() {
